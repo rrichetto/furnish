354d2c35f7a89116eb9c3058bceb46b3151e3ac1
Ryan <rrichetto@comcast.net>
2019-05-21 18:18:43 -0500

    adds slideshow functionality with JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,6 @@
+// Header shrink upon scroll
 const headerEl = document.querySelector('.header');
 
-
-// Header shrink upon scroll
 window.addEventListener('scroll', () => {
   if (window.scrollY > 10) {
     headerEl.style.padding = "1rem 0";
@@ -11,3 +10,36 @@ window.addEventListener('scroll', () => {
     headerEl.style.boxShadow = "none";
   }
 })
+
+
+// Showcase Slideshow
+const buttonEls = [...document.querySelectorAll('.slideshow__control-btn')];
+const slideEls = [...document.querySelectorAll('.slideshow__slide')];
+
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("slideshow__control-btn")) {
+
+    buttonEls.forEach(button => {
+      button.classList.remove('slideshow__control-btn--active');
+    });
+
+    e.target.classList.add('slideshow__control-btn--active');
+
+    let index = Number(e.target.dataset.index);
+    showSlide(index);
+  }
+});
+
+function showSlide(index) {
+  slideEls.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "flex";
+    } else {
+      slide.style.display = "none";
+    }
+  })
+}
+
+
+
+// ADD SET TIMOUT FUNCITON!!!
