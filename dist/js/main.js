c324d3939b7a005a92556652d09a4bdbfe116c79
Ryan <rrichetto@comcast.net>
2019-05-21 01:55:11 -0500

    adds slideshow content (but not functionality yet

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,9 @@ const headerEl = document.querySelector('.header');
 window.addEventListener('scroll', () => {
   if (window.scrollY > 10) {
     headerEl.style.padding = "1rem 0";
+    headerEl.style.boxShadow = "0 1rem 1rem rgba(0, 0, 0, .1)";
   } else {
     headerEl.style.padding = "3rem 0";
+    headerEl.style.boxShadow = "none";
   }
 })
